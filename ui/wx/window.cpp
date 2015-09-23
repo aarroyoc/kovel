177f5a46d34b427d005d8bf9a2a68374d11b16e2
Adrián Arroyo Calle <adrian.arroyocalle@gmail.com>
2015-09-23 20:26:54 +0200

    New UI layout

--- ui/wx/window.cpp
+++ ui/wx/window.cpp
@@ -4,9 +4,9 @@
 #include "window.hpp"
 #include "preview.hpp"
 
-class WorkPanel : public wxPanel{
+class ToolPanel : public wxPanel{
 	public:
-		WorkPanel(wxWindow* parent) : wxPanel(parent,wxID_ANY){
+		ToolPanel(wxWindow* parent) : wxPanel(parent,wxID_ANY){
 			
 			// Maybe wxGrid here for buttons with pictures?
 			wxGridSizer* grid=new wxGridSizer(16,2,5,5);
@@ -31,6 +31,32 @@ class WorkPanel : public wxPanel{
 		}
 };
 
+class WorkPanel : public wxPanel{
+	public:
+		WorkPanel(wxWindow* parent) : wxPanel(parent,wxID_ANY)
+		{
+			wxColour red;
+			red.Set("#ff0000");
+			wxPanel* workSide=new wxPanel(this,wxID_ANY);
+			workSide->SetBackgroundColour(red);
+			workSide->SetSize(100,100);
+			wxPanel* workTools=new wxPanel(this,wxID_ANY);
+			
+			wxButton* up=new wxButton(workTools,wxID_ANY,"Up");
+			wxButton* down=new wxButton(workTools,wxID_ANY,"Down");
+			
+			wxBoxSizer* sizer=new wxBoxSizer(wxVERTICAL);
+			sizer->Add(workSide,3,wxALIGN_CENTER | wxSHAPED);
+			sizer->Add(workTools,1,wxEXPAND | wxALL);
+			SetSizer(sizer);
+			
+			wxBoxSizer* workToolsSizer=new wxBoxSizer(wxHORIZONTAL);
+			workToolsSizer->Add(up,1,wxEXPAND | wxALL,5);
+			workToolsSizer->Add(down,1,wxEXPAND | wxALL,5);
+			workTools->SetSizer(workToolsSizer);
+		}
+};
+
 
 MainWindow::MainWindow() : wxFrame(NULL,-1,"(new file) -- Kovel - Voxel Editor",wxDefaultPosition,wxSize(800,600),wxDEFAULT_FRAME_STYLE)
 {
@@ -44,28 +70,33 @@ MainWindow::MainWindow() : wxFrame(NULL,-1,"(new file) -- Kovel - Voxel Editor",
 	
 	wxPanel* panel=new wxPanel(this,wxID_ANY);
 	
+	wxPanel* main=new wxPanel(panel,wxID_ANY);
+	wxPanel* tool=new wxPanel(panel,wxID_ANY);
+	
+	wxPanel* work=new wxPanel(main,wxID_ANY);
+	PreviewPanel* preview=new PreviewPanel(main);
+	
+	WorkPanel* workOne=new WorkPanel(work);
+	WorkPanel* workTwo=new WorkPanel(work);
+	WorkPanel* workThree=new WorkPanel(work);
+	
 	wxBoxSizer* sizer=new wxBoxSizer(wxHORIZONTAL);
-	wxBoxSizer* centralSizer=new wxBoxSizer(wxVERTICAL);
-	
-	WorkPanel* workPanel=new WorkPanel(panel);
-	wxPanel* centralPanel=new wxPanel(panel,wxID_ANY);
-	PreviewPanel* previewPanel=new PreviewPanel(centralPanel);
-	wxPanel* fillPanel=new wxPanel(centralPanel,wxID_ANY);
-	wxPanel* texturePanel=new wxPanel(panel,wxID_ANY);
-	fillPanel->SetBackgroundColour(blue);
-	texturePanel->SetBackgroundColour(yellow);
-	
-	Bind(wxEVT_PAINT,&PreviewPanel::OnPaint,previewPanel,-1);
-	
-	sizer->Add(workPanel,1,wxEXPAND | wxALL, 0);
-	sizer->Add(centralPanel,2,wxEXPAND | wxALL, 0);
-	sizer->Add(texturePanel,1,wxEXPAND | wxALL, 0);
-	
-	centralSizer->Add(previewPanel,1,wxALIGN_CENTER | wxSHAPED, 0);
-	centralSizer->Add(fillPanel,2,wxEXPAND | wxALL, 0);
-	
+	sizer->Add(main,3,wxEXPAND | wxALL);
+	sizer->Add(tool,1,wxEXPAND | wxALL);
 	panel->SetSizer(sizer);
-	centralPanel->SetSizer(centralSizer);
+	
+	wxBoxSizer* mainSizer=new wxBoxSizer(wxVERTICAL);
+	mainSizer->Add(work,1,wxEXPAND | wxALL);
+	mainSizer->Add(preview,1,wxALIGN_CENTER | wxSHAPED);
+	main->SetSizer(mainSizer);
+	
+	wxBoxSizer* workSizer=new wxBoxSizer(wxHORIZONTAL);
+	workSizer->Add(workOne,1,wxEXPAND | wxALL,5);
+	workSizer->Add(workTwo,1,wxEXPAND | wxALL,5);
+	workSizer->Add(workThree,1,wxEXPAND | wxALL,5);
+	work->SetSizer(workSizer);
+	
+	tool->SetBackgroundColour(green);
 	
 	Centre();
 
