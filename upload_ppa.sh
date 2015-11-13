0b8d7ae523873756aca7356f781d7f987ca5a899
Adrián Arroyo Calle <adrian.arroyocalle@gmail.com>
2015-11-13 18:12:53 +0100

    Samples filed moved

--- upload_ppa.sh
+++ upload_ppa.sh
@@ -5,4 +5,4 @@ dpkg-buildpackage -rfakeroot -kadrian.arroyocalle@gmail.com
 # Build Source package
 # Check CHANGELOG file
 debuild -S -sa -kadrian.arroyocalle@gmail.com
-dput ppa:adrian-arroyocalle/kovel ../kovel*source.changes
+dput ppa:adrian-arroyocalle/kovel ../kovel*source.changes # dput kovel-ppa
